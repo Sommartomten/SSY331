5a2e716c1e355cf3be9a78aa46eb7bc6b523f845
Jonatan Nyström <jonatan.nystrom@willandsfarm.se>
2024-11-06 16:29:29 +0100

    new speed to signal funktion

--- Uppgift_2_V1/speed_to_signal.ino
+++ Uppgift_2_V1/speed_to_signal.ino
@@ -1,7 +1,7 @@
-double speed_to_signalV(double speed){
-  return((speed*7.1217)+1500);
+double speed_to_signalH(double speed){
+  return(constrain(1500+27.4273508*speed -83.0335264*power(speed,2)+ 35564.5474(speed,3),1300,1700));
 
 }
-double speed_to_signalH(double speed){
-  return((speed*(-9.7109))+1500);
+double speed_to_signalV(double speed){
+  return(constrain(1500-45.7333129*speed+69.7616451*power(speed,2)-34856.56243512(speed,3),1300,1700);
 }
