d92803f72ae5c25aaa602aea38494cfa462b9e42
Jonatan Nyström <jonatan.nystrom@willandsfarm.se>
2024-11-14 11:21:44 +0100

    changed structure of folders

--- Uppgift_2_V1/speed_to_signal.ino
+++
@@ -1,7 +0,0 @@
-double speed_to_signalH(double speed){
-  return(constrain(1500+27.4273508*speed -83.0335264*power(speed,2)+ 35564.5474(speed,3),1300,1700));
-
-}
-double speed_to_signalV(double speed){
-  return(constrain(1500-45.7333129*speed+69.7616451*power(speed,2)-34856.56243512(speed,3),1300,1700);
-}
